527a5812c97bf603d2db40544364ef7b5279759f
Akshat2512 <akshat.2016603@gmail.com>
2024-11-26 00:58:06 +0530

    commit

--- app.py
+++ app.py
@@ -169,4 +169,6 @@ def save_audio_to_file(audio_data, file_path):    # save audio to the folder tem
 
 if __name__ == '__main__':
    uvicorn.run(app, host='0.0.0.0', port=5000)
+#    uvicorn.run(app, host='localhost', port=5000)
+
    
