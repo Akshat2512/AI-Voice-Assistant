8acebc7a4625ab49017568dcdd8ab7a12f55d176
Akshat2512 <akshat.2016603@gmail.com>
2024-11-25 22:51:50 +0530

    commit

--- backend/speech_proccessing.py
+++ backend/speech_proccessing.py
@@ -1,10 +1,10 @@
 
 # import wave
 import numpy as np
-import tensorflow as tf
+# import tensorflow as tf
 import zipfile
 import time
-# import tflite_runtime.interpreter as tflite
+import tflite_runtime.interpreter as tflite
 # from tflite_runtime.interpreter import Interpreter
 
 TARGET_LENGTH = 15600
