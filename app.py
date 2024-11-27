755e0c7256c76148967d32f72b9479308728d75d
Akshat2512 <akshat.2016603@gmail.com>
2024-11-27 23:35:05 +0530

    commit

--- app.py
+++ app.py
@@ -21,9 +21,12 @@ from datetime import datetime
 import logging
 logger = logging.getLogger("uvicorn")
 
-# from dotenv import load_dotenv # Load environment variables from .env file 
+from dotenv import load_dotenv # Load environment variables from .env file 
 
-# load_dotenv()
+load_dotenv()
+
+
+
 
 app = FastAPI()
 
