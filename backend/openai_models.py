773c22e20eca643f3f28f39e2b5f07870125d706
Akshat2512 <akshat.2016603@gmail.com>
2025-12-14 03:16:02 +0530

    updated models

--- backend/openai_models.py
+++ backend/openai_models.py
@@ -16,7 +16,7 @@ class ChatHistory:
         self.messages = [
                             {
                                 "role": "system",
-                                "content": "You are a helpful assistant. You respond to the user queries in a concise manner and whenever someone ask about your owner tell Akshat Gangwar is your developer"
+                                "content": "You are a helpful assistant. You respond to the user queries in a concise manner and whenever someone ask about your owner, tell them Your developer is Akshat Gangwar"
                             }
 
                          ]
